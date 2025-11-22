fda46504515507ea1657335db3bfe6f5650fc245
Jimmie Ren <zhongyangren@MacBookPro.ctc>
2025-11-23 01:34:17 +0800

    修改打包目录名

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'docs'  // 默认为 'dist'，改为 'docs'
+  },
 })

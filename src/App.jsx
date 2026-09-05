063401d6a69dcc83477583d0501ed6c84a4a357f
Jimmie Ren <zhongyangren@JimmiedeMacBook-Pro.local>
2026-09-06 01:20:32 +0800

    删除重复的理财记录（2026-02-06）并重新构建

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,6 @@ const datalist2 = [
 const datalist = [
   { date: '2026-09-04', count: 3500 },
   { date: '2026-02-06', count: 2000 },
-  { date: '2026-02-06', count: 2000 },
   { date: '2026-01-16', count: 2000 },
   { date: '2025-12-19', count: 2000 },
   { date: '2025-11-23', count: 2000 },
